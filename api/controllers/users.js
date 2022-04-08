c67713dbbc39f3950a9e908ce00ee0fe234554f7
sidaroz <sidarozbek96@gmail.com>
2022-04-08 19:11:56 +0100

    user can be found if correct username & password is entered, (this is without encryption which will be added)

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,14 +1,29 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
 async function createUser(req, res) {
-    try {
-        // const salt = await bcrypt.genSalt();
-        // const hashed = await bcrypt.hash(req.body.password_set, salt)
-        await User.createUser({...req.body})
-        res.status(201).json({msg: 'User created!'})
-    } catch (error) {
-        res.status(500).json({err: 'User cannot be created'})
-    }
+  try {
+    // const salt = await bcrypt.genSalt();
+    // const hashed = await bcrypt.hash(req.body.password_set, salt)
+    await User.createUser({ ...req.body });
+    res.status(201).json({ msg: "User created!" });
+  } catch (error) {
+    res.status(500).json({ err: "User cannot be created" });
+  }
 }
 
-module.exports = { createUser };
+async function findByUsername(req, res) {
+  try {
+    const user = await User.findByUsername(req.body.username);
+    if (!user) {
+      throw new Error("No user with this email");
+    } else if (
+      user.username === req.body.username &&
+      user.password_set === req.body.password_set
+    ) {
+      res.status(200).json({ msg: "User signed in" });
+    }
+  } catch (error) {
+    res.status(401).json({ err: "User could not be logged in" });
+  }
+}
+module.exports = { createUser, findByUsername };
