0516dd908bd3a324f93c9aac639637b3adf21519
Jaxsan Sivanesan <jaxsan.sivanesan@gmail.com>
2022-04-10 14:52:15 +0100

    Worked on the feed page and able to send a request to the backend to add a habit

--- Client/Static/js/content.js
+++ Client/Static/js/content.js
@@ -120,15 +120,56 @@ function renderRegisterForm() {
 
 
 async function renderFeed() {
-    body.innerHTML = `<nav class="navbar navbar-light bg-dark py-3">
+    body.innerHTML = `<nav class="navbar navbar-light bg-dark py-3" >
     <div class="container-fluid">
-        <a class="navbar-brand" style="margin:auto; color:white">Group Name</a>
+        <button class="btn btn-primary" onclick="openTab('b1');">Add Habit</button>
+        <a class="navbar-brand" >Group Name</a>
+        <a href="" class="btn btn-primary">Log out</a>
     </div>
 </nav>
-
+<div id="b1" class="containerTab">
+    <!-- If you want the ability to close the container, add a close button -->
+    <span onclick="this.parentElement.style.display='none'" class="closebtn">x</span>
+    <form class="padding" >
+        <div>
+             <h2 class="addHabitTitle">Add Habit</h2>
+        </div>
+        <div>
+            <input type="text" id="" class="form-control" name="Habit-Name" placeholder="Enter Habit Name" required>
+        </div>
+        <div class="py-3" class="form-control">
+            <input type="number" id="" class="form-control" name="Repetitions" placeholder="# of Repetitions" required>
+        </div>  
+        <div class="form-group" >
+            <label for="sel1" class="frequency">Frequency:</label>
+            <select class="form-control" id="sel1" name="Frequency">
+                <option>Daily</option>
+                <option>Weekly</option>
+                <option>Monthly</option>
+                <option>Yearly</option>
+            </select>
+        </div>
+        <div style="padding-top:10px">
+            <button type="submit" class="btn btn-primary" id="add-habitsubmit" style="width:100%">Add Habit</button> 
+        </div>
+    </form>
+</div>
 <section>
-    <h1>You have now entered the feed</h1>
-</section>
+<h1 style="text-align:center">View all Habits Below</h1>
+
+<div style="display:flex; padding:10px;"> 
+    <button class="btn btn-primary">+</button>
+
+    <div style="display:flex; background:rgb(33,37,41,0.8); color:white; padding:0.5rem; border-radius: 10px; margin:0px 10px; width:100%; justify-content: center;" class="container">
+        <div class="row">
+            <div class="col">3/8 Daily</div> 
+            <div class="col" >Drink Water</div>
+            <div class="col">🔥0</div>
+        </div>
+    </div>
+    <button class="btn btn-primary">x</button>
+</div>
+</section>  
 <div class="container absolute bg-dark" >
     <footer class="my-4">
       <ul class="nav justify-content-center border-bottom pb-3 mb-3">
@@ -140,6 +181,10 @@ async function renderFeed() {
       <p class="text-center"  style="color:white">© 2022 GroupName, Inc</p>
     </footer>
 </div>`
+
+    const addHabitForm = document.querySelector('form')
+    addHabitForm.addEventListener('submit', submitHabit)
+    
 }
 
 
